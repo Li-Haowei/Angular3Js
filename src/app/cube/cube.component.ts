3a7aee84132e190c46bd43870b5b9a043b59ac0b
Li-Haowei <haoweilee666@gmail.com>
2022-04-25 17:23:56 -0400

    comments and mapping earch

--- src/app/cube/cube.component.ts
+++ src/app/cube/cube.component.ts
@@ -7,7 +7,10 @@ import * as THREE from "three";
   styleUrls: ['./cube.component.scss']
 })
 export class CubeComponent implements OnInit, AfterViewInit {
-
+  //ViewChild is a property decorator that configures a view query. The change detector looks for the 
+  //first element or the ddirective matching the selector in the view DOM. If the view DOM changes, 
+  //and a new child matches the selector, the property is updated
+  //https://angular.io/api/core/ViewChild
   @ViewChild('canvas')
   private canvasRef: ElementRef;
 
@@ -19,28 +22,36 @@ export class CubeComponent implements OnInit, AfterViewInit {
 
   @Input() public size: number = 200;
 
-  @Input() public texture: string = "/assets/texture.jpg";
+  @Input() public texture: string = "/assets/earth.jpeg"; //This is for texture mapping
 
 
-  //* Stage Properties
+  //* Stage Properties, 
+  // referring to /assets/camera_view.png
 
-  @Input() public cameraZ: number = 400;
+  @Input() public cameraZ: number = 600; //Camera position on the z-axis
 
-  @Input() public fieldOfView: number = 1;
+  @Input() public fieldOfView: number = 1; //zoom control, higher the further
 
   @Input('nearClipping') public nearClippingPlane: number = 1;
 
-  @Input('farClipping') public farClippingPlane: number = 1000;
+  @Input('farClipping') public farClippingPlane: number = 1000; //This is the background, black canvas
 
   //? Helper Properties (Private Properties);
 
-  private camera!: THREE.PerspectiveCamera;
+  private camera!: THREE.PerspectiveCamera; //It defines a frustum, feeling of distance in 3d views
 
   private get canvas(): HTMLCanvasElement {
     return this.canvasRef.nativeElement;
   }
   private loader = new THREE.TextureLoader();
-  private geometry = new THREE.BoxGeometry(1, 1, 1);
+//Geometry is what defines the shape, and there are other basic predefined option
+//Look at assets/options.jpeg
+  //private geometry = new THREE.BoxGeometry(1, 1, 1);
+  private geometry = new THREE.SphereGeometry(1.5, 32, 32);
+//This is what loads the texture, and it can load color like:
+/*new THREE.MeshBasicMaterial({
+  color: 0xFF8001
+})*/
   private material = new THREE.MeshBasicMaterial({ map: this.loader.load(this.texture) });
 
   private cube: THREE.Mesh = new THREE.Mesh(this.geometry, this.material);
@@ -51,18 +62,18 @@ export class CubeComponent implements OnInit, AfterViewInit {
 
   /**
    *Animate the cube
-   *
+   *and positioning a Mesh
    * @private
    * @memberof CubeComponent
    */
   private animateCube() {
-    this.cube.rotation.x += this.rotationSpeedX;
+    this.cube.position.set(2,0,0);
+    //this.cube.rotation.x += this.rotationSpeedX;
     this.cube.rotation.y += this.rotationSpeedY;
   }
 
   /**
    * Create the scene
-   *
    * @private
    * @memberof CubeComponent
    */
@@ -100,6 +111,7 @@ export class CubeComponent implements OnInit, AfterViewInit {
     this.renderer.setSize(this.canvas.clientWidth, this.canvas.clientHeight);
 
     let component: CubeComponent = this;
+    //recursive render function to generate the animation loop
     (function render() {
       requestAnimationFrame(render);
       component.animateCube();
@@ -112,7 +124,7 @@ export class CubeComponent implements OnInit, AfterViewInit {
   ngOnInit(): void {
 
   }
-
+  //finally, create scene
   ngAfterViewInit() {
     this.createScene();
     this.startRenderingLoop();
